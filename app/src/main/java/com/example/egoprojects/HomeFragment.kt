4440b06dded2e2d37fa7a449535711894ffba61a
Nebican Planalı <nebicanplanali@gmail.com>
2024-08-27 05:09:14 +0300

    Fragment home created, BottomNav completed

--- app/src/main/java/com/example/egoprojects/HomeFragment.kt
+++ app/src/main/java/com/example/egoprojects/HomeFragment.kt
@@ -7,11 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class HappinesFragment : Fragment() {
+class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_happines, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
--- app/src/main/java/com/example/egoprojects/MainActivity.kt
+++ app/src/main/java/com/example/egoprojects/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.egoprojects.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
